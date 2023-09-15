3c0a98c72db8a3a627b38469b737897358028ac1
mio818 <is0720hi@ed.ritsumei.ac.jp>
2023-09-15 20:52:34 +0900

    みんなで色々レイアウトを変えた

--- src/components/Audio/audio.js
+++ src/components/Audio/audio.js
@@ -3,99 +3,103 @@ import { Link, useLocation } from "react-router-dom";
 import styles from "./Audio.module.css";
 
 const Audio = () => {
-	const location = useLocation();
-	const { imageData } = location.state;
+  const location = useLocation();
+  const { imageData } = location.state;
 
-	const [volume, setVolume] = useState(0);
-	// 変数
-	var audioContext;
-	var mediaStreamSource;
-	var meter;
-	var maxi = 0;
+  const [volume, setVolume] = useState(0);
 
-	// メーターの生成
-	function createAudioMeter(audioContext, clipLevel, averaging, clipLag) {
-		// メーターの生成
-		const processor = audioContext.createScriptProcessor(512);
-		processor.onaudioprocess = volumeAudioProcess;
-		processor.clipping = false;
-		processor.lastClip = 0;
-		processor.volume = 0;
-		processor.clipLevel = clipLevel || 0.98;
-		processor.averaging = averaging || 0.95;
-		processor.clipLag = clipLag || 750;
-		processor.connect(audioContext.destination);
+  // 変数
+  var audioContext;
+  var mediaStreamSource;
+  var meter;
+  var maxi = 0;
 
-		// クリップチェック時に呼ばれる
-		processor.checkClipping = function () {
-			if (!this.clipping) {
-				return false;
-			}
-			if (this.lastClip + this.clipLag < window.performance.now()) {
-				this.clipping = false;
-			}
-			return this.clipping;
-		};
+  // メーターの生成
+  function createAudioMeter(audioContext, clipLevel, averaging, clipLag) {
+    // メーターの生成
+    const processor = audioContext.createScriptProcessor(512);
+    processor.onaudioprocess = volumeAudioProcess;
+    processor.clipping = false;
+    processor.lastClip = 0;
+    processor.volume = 0;
+    processor.clipLevel = clipLevel || 0.98;
+    processor.averaging = averaging || 0.95;
+    processor.clipLag = clipLag || 750;
 
-		// シャットダウン時に呼ばれる
-		processor.shutdown = function () {
-			this.disconnect();
-			this.onaudioprocess = null;
-		};
+    processor.connect(audioContext.destination);
+    // クリップチェック時に呼ばれる
+    processor.checkClipping = function () {
+      if (!this.clipping) {
+        return false;
+      }
+      if (this.lastClip + this.clipLag < window.performance.now()) {
+        this.clipping = false;
+      }
+      return this.clipping;
+    };
 
-		return processor;
-	}
+    // シャットダウン時に呼ばれる
+    processor.shutdown = function () {
+      this.disconnect();
+      this.onaudioprocess = null;
+    };
 
-	// オーディオ処理時に呼ばれる
-	function volumeAudioProcess(event) {
-		const buf = event.inputBuffer.getChannelData(0);
-		const bufLength = buf.length;
-		let sum = 0;
-		let x;
+    return processor;
+  }
 
-		// 平均ボリュームの計算
-		for (var i = 0; i < bufLength; i++) {
-			x = buf[i];
-			if (Math.abs(x) >= this.clipLevel) {
-				this.clipping = true;
-				this.lastClip = window.performance.now();
-			}
-			sum += x * x;
-		}
-		const rms = Math.sqrt(sum / bufLength);
-		this.volume = Math.max(rms, this.volume * this.averaging);
+  // オーディオ処理時に呼ばれる
+  function volumeAudioProcess(event) {
+    const buf = event.inputBuffer.getChannelData(0);
+    const bufLength = buf.length;
+    let sum = 0;
+    let x;
 
-		maxi = Math.max(this.volume, maxi);
-		setVolume(maxi);
-		//setVolume(this.volume);
+    // 平均ボリュームの計算
+    for (var i = 0; i < bufLength; i++) {
+      x = buf[i];
+      if (Math.abs(x) >= this.clipLevel) {
+        this.clipping = true;
+        this.lastClip = window.performance.now();
+      }
+      sum += x * x;
+    }
+    const rms = Math.sqrt(sum / bufLength);
+    this.volume = Math.max(rms, this.volume * this.averaging);
 
-		// ボリュームの表示
-		// output.innerHTML = "ボリューム: " + this.volume.toFixed(4);
-	}
+    maxi = Math.max(this.volume, maxi);
+    setVolume(maxi);
+    //setVolume(this.volume);
 
-	// ボリューム検出の開始
-	function beginDetect() {
-		// 色の変更
-		const Button = document.getElementById("but");
-		Button.style.backgroundColor = "red";
-		// オーディオストリームの生成
-		audioContext = new (window.AudioContext || window.webkitAudioContext)();
-		// 音声入力の開始
-		if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
-			navigator.mediaDevices.getUserMedia({ audio: true }).then((stream) => {
-				// const mediaStream = navigator.mediaDevices.getUserMedia({ audio: true });
-				// mediaStream.then((stream) => {
-				// メディアストリームソースとメーターの生成
-				mediaStreamSource = audioContext.createMediaStreamSource(stream);
-				meter = createAudioMeter(audioContext);
-				mediaStreamSource.connect(meter);
-				console.log("sss");
-			});
-		}
-	}
+    // ボリュームの表示
+    // output.innerHTML = "ボリューム: " + this.volume.toFixed(4);
+  }
 
-	return (
-		/*<head>
+  // ボリューム検出の開始
+  function beginDetect() {
+    // 色の変更
+    const Button = document.getElementById("but");
+    Button.style.backgroundColor = "red";
+    // オーディオストリームの生成
+    audioContext = new (window.AudioContext || window.webkitAudioContext)();
+
+    // 音声入力の開始
+    if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+      navigator.mediaDevices.getUserMedia({ audio: true }).then((stream) => {
+        // const mediaStream = navigator.mediaDevices.getUserMedia({ audio: true });
+        // mediaStream.then((stream) => {
+        // メディアストリームソースとメーターの生成
+        mediaStreamSource = audioContext.createMediaStreamSource(stream);
+        meter = createAudioMeter(audioContext);
+        mediaStreamSource.connect(meter);
+
+        console.log("sss");
+      });
+    }
+    // setTimeout(audioContext.close(), 2000);
+  }
+
+  return (
+    /*<head>
 			<meta charset="UTF-8"></meta>
 			<meta
 				name="viewport"
@@ -118,84 +122,85 @@ const Audio = () => {
 			></link>
 		</head>*/
 
-		/*<div>*/
-		<div>
-			<head>
-				<meta charset="UTF-8"></meta>
-				<meta
-					name="viewport"
-					content="width=device-width, initial-scale=1.0"
-				></meta>
+    /*<div>*/
+    <div>
+      <head>
+        <meta charset="UTF-8"></meta>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0"
+        ></meta>
 
-				<meta name="viewport" content="width=device-width"></meta>
-				{/* <!--googlefonts--> */}
-				<link rel="preconnect" href="https://fonts.googleapis.com"></link>
-				<link
-					rel="preconnect"
-					href="https://fonts.gstatic.com"
-					crossorigin
-				></link>
-				<link
-					href="https://fonts.googleapis.com/css2?family=Chokokutai&family=Reggae+One&family=Yuji+Hentaigana+Akebono&display=swap"
-					rel="stylesheet"
-				></link>
+        <meta name="viewport" content="width=device-width"></meta>
+        {/* <!--googlefonts--> */}
+        <link rel="preconnect" href="https://fonts.googleapis.com"></link>
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossorigin
+        ></link>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Chokokutai&family=Reggae+One&family=Yuji+Hentaigana+Akebono&display=swap"
+          rel="stylesheet"
+        ></link>
 
-				<link rel="preconnect" href="https://fonts.googleapis.com"></link>
-				<link
-					rel="preconnect"
-					href="https://fonts.gstatic.com"
-					crossorigin
-				></link>
-				<link
-					href="https://fonts.googleapis.com/css2?family=Chokokutai&family=Reggae+One&display=swap"
-					rel="stylesheet"
-				></link>
-				<title>uppun bakuhatu</title>
-			</head>
+        <link rel="preconnect" href="https://fonts.googleapis.com"></link>
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossorigin
+        ></link>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Chokokutai&family=Reggae+One&display=swap"
+          rel="stylesheet"
+        ></link>
+        <title>uppun bakuhatu</title>
+      </head>
 
-			<body>
-				<header>
-					<div class={styles.background}></div>
-				</header>
+      <body>
+        <header>
+          <div class={styles.background}></div>
+        </header>
 
-				<section>
-					<div class={styles.button}>
-						<Link to="/UploadPage">
-							<p class={styles.button_top}>戻る</p>
-						</Link>
-					</div>
-				</section>
+        <section>
+          <div class={styles.button}>
+            <Link to="/UploadPage">
+              <p class={styles.button_top}>戻る</p>
+            </Link>
+          </div>
+        </section>
 
-				<div class={styles.shout}>
-					<p>叫べ！</p>
-				</div>
-				<div class={styles.voice}></div>
-				<div class={styles.REC} onClick={beginDetect}>
-					<div class={styles.text}>REC</div>
-					<div id={styles.box} class={styles.color}>
-						<button id="but"></button>
-					</div>
-				</div>
-				<div class={styles.wrapper}>
-					<Link
-						to="/UploadPage/ImageRecievege"
-						state={{
-							imageData: imageData,
-							volume: Math.floor(volume * Math.pow(10, 2)) / Math.pow(10, 2),
-						}}
-					>
-						終了
-					</Link>
-				</div>
-				<div class={styles.volumevalue}>
-					<p>ボリューム: {volume.toFixed(2) * 200} 点</p>
-				</div>
-				{/* <button onClick={""}>stop</button> */}
-			</body>
-			{/* <button onClick={beginDetect}>ボリューム検出の開始</button> */}
+        <div class={styles.shout}>
+          <p>叫べ！</p>
+        </div>
+        <div class={styles.voice}></div>
+        <div class={styles.REC} onClick={beginDetect}>
+          <div class={styles.text}>REC</div>
+          <div id={styles.box} class={styles.color}>
+            <button id="but"></button>
+          </div>
+        </div>
+        <div class={styles.wrapper}>
+          <Link
+            to="/UploadPage/ImageRecievege"
+            state={{
+              imageData: imageData,
+              volume: Math.floor(volume * Math.pow(10, 2)) / Math.pow(10, 2),
+            }}
+          >
+            終了
+          </Link>
+        </div>
+        {/* <button onClick={stopDetect}>あああ</button> */}
+        <div class={styles.volumevalue}>
+          <p>ボリューム: {volume.toFixed(2) * 200} 点</p>
+        </div>
+        {/* <button onClick={""}>stop</button> */}
+      </body>
+      {/* <button onClick={beginDetect}>ボリューム検出の開始</button> */}
 
-			{/* <img src={imageData}></img> */}
-		</div>
-	);
+      {/* <img src={imageData}></img> */}
+    </div>
+  );
 };
 export default Audio;
--- src/components/Score/Score.js
+++ src/components/Score/Score.js
@@ -1,14 +1,55 @@
+import React from "react";
 import { useLocation } from "react-router-dom";
+import styles from "./Score.module.css";
 
 const Score = () => {
-	const location = useLocation();
-	const { volume } = location.state;
+  const location = useLocation();
+  const { volume } = location.state;
 
-	return (
-		<>
-			<h1>{volume.volume * 10}点</h1>
-		</>
-	);
+  return (
+    <>
+      <h1>{volume.volume * 10}点</h1>
+    </>
+  );
 };
 
 export default Score;
+
+/*const uploadTime = onFileChange(e);
+
+return (
+  <>
+    <head>
+      <meta charset="UTF-8"></meta>
+      <meta
+        name="viewport"
+        content="width=device-width, initial-scale=1.0"
+      ></meta>
+
+      <link rel="stylesheet" type="text/css" href="style.css"></link>
+      <meta name="viewport" content="width=device-width"></meta>
+      <title>Document</title>
+
+      <link rel="preconnect" href="https://fonts.googleapis.com"></link>
+      <link
+        rel="preconnect"
+        href="https://fonts.gstatic.com"
+        crossorigin
+      ></link>
+      <link
+        href="https://fonts.googleapis.com/css2?family=Chokokutai&family=Reggae+One&display=swap"
+        rel="stylesheet"
+      ></link>
+    </head>
+
+    <body>
+      <Link to="/">
+        <p class={styles.button_top}>戻る</p>
+      </Link>
+      <div class="bestScore">ベストスコア</div>
+      <ul id="photoList"></ul>
+      <div id="topRanks"></div>
+    </body>
+  </>
+);
+*/
